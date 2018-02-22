cb719a4792d61b3ade6e4db58a82ba77cd4ae2c9
ABS19stein <abs19stein@gmail.com>
2018-02-22 17:43:03 -0600

    added user authentication functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get '/' => 'tasks#index'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get '/logout' => 'sessions#delete'
+
+
 end
