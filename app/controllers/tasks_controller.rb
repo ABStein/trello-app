8989b6833c4e979c03eff39a8524ea1cad564d47
Andrew Stein <ABStein@github.com>
2019-08-26 20:05:03 -0500

    added controller logic for crud operations, functionality to create a task, updated gems

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,46 @@
 class TasksController < ApplicationController
   def index
-    @task = Task.first
+    @tasks = Task.all
+    @home_page = true
+  end
+
+  def new
+
+  end
+  
+  def create
+    task = Task.new(
+      id: params[:id],
+      title: params[:title],
+      description: params[:description]
+    )
+
+    task.save
+    redirect_to '/'
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit 
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+
+    @task.assign_attributes(
+      id: params[:id],
+      title: params[:title],
+      description: params[:description]
+    )
+    @task.save
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to "/"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get '/' => 'tasks#index'
+  get '/tasks/new' => 'tasks#new'
+  post '/tasks' => 'tasks#create'
+  get '/tasks/:id' => 'tasks#show'
+  get 'tasks/:id/edit' => 'tasks#edit'
+  get 'tasks/:id/update' => 'tasks#update'
+  get 'tasks/:id' => 'tasks#destroy'
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
@@ -7,6 +13,5 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get '/logout' => 'sessions#delete'
-
 
 end
